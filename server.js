b7ae4bfc41ab50ca1289cddd7b093907f37e4446
Mike Smart <mike.smart@jadu.co.uk>
2016-09-19 20:04:04 +0100

    cleaning output dir once bundle request is sent rather than on boot, ensures a clean output dir on each instance

--- server.js
+++ server.js
@@ -23,70 +23,23 @@ app.use(cors());
 app.use(express.static(PUBLIC));
 
 /**
-* Simple CLI for quick debugging
-**/
-if (process.argv[2] === 'debug') {
-    let test;
-    if (process.argv[3] === 'norwich') {
-        test = new Staticify({
-            requestUri: 'https://www.norwich.gov.uk/info',
-            targetUri: 'http://mike.com',
-            removeMainContent: false,
-            assetPath: 'site',
-        }, eventEmitter);
-    }
-    else if (process.argv[3] === 'jsna') {
-        test = new Staticify({
-            requestUri: 'https://www.jsna.centralbedfordshire.gov.uk',
-            targetUri: 'http://mike.com',
-            removeMainContent: true
-        }, eventEmitter);
-    }
-    else if (process.argv[3] === 'birmingham') {
-        test = new Staticify({
-            requestUri: 'https://www.birmingham.gov.uk/events',
-            assetPath: 'site',
-            targetUri: 'http://mike.com',
-            removeMainContent: false
-        }, eventEmitter);
-    }
-    else if (process.argv[3]) {
-        test = new Staticify({
-            requestUri: process.argv[3],
-            targetUri: 'http://mike.com',
-            assetPath: process.argv[4] ? process.argv[4] : 'debug/site',
-            verbose: process.argv[5] ? true : false,
-            removeMainContent: false
-        }, eventEmitter);
-    }
-    else {
-        test = new Staticify({
-            requestUri: 'http://localhost:3000/debug/site/index.html',
-            targetUri: 'http://mike.com',
-            removeMainContent: false
-        }, eventEmitter);
-    }
-
-    test.initiate();
-}
-else if (process.argv[2] === 'clean') {
-    removeDirs(process.exit);
-}
-
-/**
  * Recursively remove output directory
  **/
 
-function removeDirs (exit = null) {
+function cleanOutput (callback = null) {
     const outputLocation = `${__dirname}/site/public/output/`;
     const outputZip = `${__dirname}/site/public/output_bundle.zip`;
 
     rimraf(outputLocation, {}, () => {
         console.log('removed output directory');
-    });
 
-    rimraf(outputZip, {}, () => {
-        console.log('removed output zip');
+        rimraf(outputZip, {}, () => {
+            console.log('removed output zip');
+
+            if (callback) {
+                callback();
+            }
+        });
     });
 }
 
@@ -97,20 +50,21 @@ function removeDirs (exit = null) {
 http.listen(PORT, () => {
     console.log(`App listening on ${PORT}`);
     console.log(`Serving from ${PUBLIC}`);
-    removeDirs();
 });
 
 io.on('connection', (socket) => {
     socket.on('request bundle', (data) => {
-        const { requestUri, fileName, redirectUri, assetPath } = data;
-        const bundle = new Staticify({
-            requestUri: requestUri,
-            assetPath: assetPath,
-            outputFile: fileName,
-            targetUri: redirectUri,
-            verbose: true
-        }, eventEmitter, io).initiate();
-
         io.emit('status', 'Server received request');
+
+        cleanOutput(() => {
+            const { requestUri, fileName, redirectUri, assetPath } = data;
+            const bundle = new Staticify({
+                requestUri: requestUri,
+                assetPath: assetPath,
+                outputFile: fileName,
+                targetUri: redirectUri,
+                verbose: true
+            }, eventEmitter, io).initiate();
+        });
     });
 });
